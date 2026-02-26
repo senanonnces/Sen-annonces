a89b43d50c041197c19a3c234639151bc846d908
senanonnces <ahmadawadbut@gmail.com>
2026-02-26 23:09:36 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/splash_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://hzmfxjpxejjmqzfjtsck.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6bWZ4anB4ZWpqbXF6Zmp0c2NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1MTQ3MzgsImV4cCI6MjA1NjA5MDczOH0.Vo3X56d4mqdnSe3xJCjD56nqzqm2g0VjMmfF8eRjpgU',
+  );
   runApp(const SenAnnoncesApp());
 }
 
 class SenAnnoncesApp extends StatelessWidget {
   const SenAnnoncesApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
